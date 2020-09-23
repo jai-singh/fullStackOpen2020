20ab48c02214be5196465c3690ce61ad141aabe4
Jai Singh <jaisingh8464@gmail.com>
2020-09-23 23:41:44 +0530

    solved unicafe step 1

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Button = ({handleClick, text}) => {
+  return(
+  <button onClick={handleClick}>{text}</button>
+  )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Display = ({text, score}) => {
+  return(
+  <p>{text} {score}</p>
+  )
+}
+
+const App = () => {
+  const [good,setGood] = useState(0)
+  const [neutral,setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const giveGood = () => setGood(good+1)
+  const giveNeutral = () => setNeutral(neutral+1)
+  const giveBad = () => setBad(bad+1)
+
+  return(
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={giveGood} text='good' />
+      <Button handleClick={giveNeutral} text='neutral'/>
+      <Button handleClick={giveBad} text='bad'/>
+      <h1>statistics</h1>
+      <Display text='good' score={good}/>
+      <Display text='neutral' score={neutral}/>
+      <Display text='bad' score={bad}/>
+    </div>
+  )
+}
+
+
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
