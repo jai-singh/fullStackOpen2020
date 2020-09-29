2fc9fe4457da19059ccb7159ac8d441337d1286d
Jai Singh <jaisingh8464@gmail.com>
2020-09-30 03:58:22 +0530

    completed 2.1, 2.2, 2.3

---
+++ part2/courseinformation/src/components/Course.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const Header = ({ course }) => {
+    return (
+      <h1>{course.name}</h1>
+    )
+}
+  
+const Total = ({ course }) => {
+    console.log(course);
+    const total = course.parts.reduce((sum,courses) => sum=sum+courses.exercises,0)
+    
+    return(
+      <p><strong>total of {total} exercises</strong></p>
+    ) 
+}
+  
+const Part = (props) => {
+    return (
+      <p>
+        {props.part.name} {props.part.exercises}
+      </p>    
+    )
+}
+  
+const Content = ({ course }) => {
+    return (
+      <div>
+        <Part part={course.parts[0]} />
+        <Part part={course.parts[1]} />
+        <Part part={course.parts[2]} />
+      </div>
+    )
+}
+
+const Course = ({course}) => {
+    return(
+        <div>
+            <Header course={course}/>
+            <Content course={course}/>
+            <Total course={course}/>
+        </div>
+    )
+}
+
+export default Course
